9b36adac383c54f0bb3dd95435a7697ab3057486
dbanda99 <ldavidbandal@gmail.com>
2026-08-06 17:57:53 -0500

    0.0.2

--- guest-api-config.js
+++ guest-api-config.js
@@ -1,5 +1,5 @@
 // Add Amanda's RSVP endpoint here once the new guest list is ready.
-window.GUEST_API_URL = "";
+window.GUEST_API_URL = "https://script.google.com/macros/s/AKfycbwUVNo00_zcySbaioENPI3Gu3CiuYN7S5nD7naGm7skY8_Ou0UDK3h8Whpn6RPtVmwT/exec";
 
 // Live Server testing only: this guest is shown when no ?guest= value is present.
 window.GUEST_API_LOCAL_DEFAULT_GUEST = "";
